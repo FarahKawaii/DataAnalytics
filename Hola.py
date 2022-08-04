e1e6831eb8bfe9e3eed42571c57fabb2a49255c3
FarahKawaii <108349742+FarahKawaii@users.noreply.github.com>
2022-08-04 22:02:30 +0800

    Update Hola.py

--- Hola.py
+++ Hola.py
@@ -3,7 +3,23 @@ import numpy as np
 import pandas as pd
 
 st.header("My first Streamlit App")
-st.write(pd.DataFrame({
-    'Intplan': ['yes', 'yes', 'yes', 'no'],
-    'Churn Status': [0, 0, 0, 1]
-}))
+
+option = st.sidebar.selectbox(
+    'Choose an option',
+     ['Greet', 'Display a picture'])
+
+
+if option == 'Greet':
+    name = st.text_input('What is your name?')
+    if name == '':
+        st.write('')
+    else:
+        st.write(f'# Hello, {name}, have a great day!')
+        
+
+else:
+    try:
+        url = st.text_input('Insert the URL of the image')
+        st.image(url)
+    except:
+        st.error('Please paste a URL')
